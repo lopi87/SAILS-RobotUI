59b55f891ced2d1216007b78a453a9b52b8b7298
manuel <lopez.urbina.manuel@gmail.com>
2016-12-09 23:28:16 +0100

    Internationalization: errors solved

--- api/controllers/LanguageController.js
+++ api/controllers/LanguageController.js
@@ -9,18 +9,7 @@ module.exports = {
 
   change: function (req, res, next) {
 
-    /*
-    if (req.xhr) {
-      if(req.param('locale') != sails.config.i18n.defaultLocale){
-        req.setLocale(req.param('locale'));
-        //req.session.languagePreference = req.param('locale');
-      }
-    }
     return res.ok({locale: req.getLocale() });
-   */
-    return res.ok({locale: req.getLocale() });
-
-
 
   }
 };
--- api/policies/localize.js
+++ api/policies/localize.js
@@ -1,37 +1,12 @@
 // api/policies/localize.js
 
-/*
 module.exports = function(req, res, next) {
 
-
-  var antes = req.getLocale();
-
-  if(req.param('locale') != sails.config.i18n.defaultLocale){
-    req.setLocale(req.param('locale'));
-    req.session.languagePreference = req.param('locale');
+  if (req.session.languagePreference == undefined){
+    req.session.languagePreference = sails.config.i18n.defaultLocale;
   }
 
-
-  var despues = req.getLocale();
-
-  //res.setLocale(req.param('locale') || sails.config.i18n.defaultLocale);
-  var a = sails.config.i18n.defaultLocale;
-  var b = req.param('locale');
-
-//  res.setLocale('en');
-
-  next();
-};
-
-*/
-
-
-// api/policies/localize.js
-module.exports = function(req, res, next) {
-
-  req.session.languagePreference = sails.config.i18n.defaultLocale;
-
-  if( req.param('locale') != req.session.languagePreference){
+  if(req.param('locale') != req.session.languagePreference && req.param('locale') != undefined){
     req.session.languagePreference = req.param('locale');
   }
 
