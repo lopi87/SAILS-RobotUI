de0a86f72bc6e7d1aad9da574c4d99c5fa270871
manuel <lopez.urbina.manuel@gmail.com>
2017-01-15 21:16:27 +0100

    Client

--- config/env/production.js
+++ config/env/production.js
@@ -25,14 +25,14 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: process.env.PORT || 5000,
+  port: 5000,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  log: {
-     level: "silent"
-  }
+  //log: {
+  //  level: "silent"
+  //}
 
 };
