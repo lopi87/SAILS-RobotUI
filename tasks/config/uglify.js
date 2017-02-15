cdfe5578dfed4e22e194375c7602c6acb6dd21bb
Manuel López Urbina <m_lopez_servext_rsi@cajarural.com>
2017-02-15 18:45:28 +0100

    Uglify configuration

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -11,12 +11,16 @@
  */
 module.exports = function(grunt) {
 
-	grunt.config.set('uglify', {
-		dist: {
-			src: ['.tmp/public/concat/production.js'],
-			dest: '.tmp/public/min/production.min.js'
-		}
-	});
+  grunt.config.set('uglify', {
+    //added this option to make uglyfication faster
+    options: {
+      compress: false
+    },
+    dist: {
+      src: ['.tmp/public/concat/production.js'],
+      dest: '.tmp/public/min/production.min.js'
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 };
