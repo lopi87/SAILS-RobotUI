10ef12ba8c63d26b035f0fa9103be4d2d8d25789
Manuel López Urbina <m_lopez_servext_rsi@cajarural.com>
2017-03-17 16:32:32 +0100

    Raspberry client l298n working

--- client/raspberry.js
+++ client/raspberry.js
@@ -10,11 +10,11 @@ var sys = require('util'), exec = require('child_process').exec,
 var path = '/sys/class/gpio/',
 
 // Pin numbers on the Raspberry Pi connected to the car's remote.
-pins = [7, 8, 9, 11];
+  pins = [2, 3, 4, 14];
 
 // Enable sending signals to the car's remote control
 // which is connected to the Raspberry Pi.
-//initPins()
+initPins()
 
 
 console.log('Waiting connection...');
@@ -47,54 +47,51 @@ io.sockets.on('connection', function (socket)
 
     // Choose the right command for sending a signal to the car's remote.
     command = '';
-    if ( data === 'UP' )
-      console.log(data);
-    //command = 'echo 1 > ' + path + 'gpio7/value'
-    else if ( data === 'DOWN' )
-    //command = 'echo 1 > ' + path + 'gpio8/value'
-      console.log(data);
-    else if ( data === 'LEFT' )
-    //command = 'echo 1 > ' + path + 'gpio9/value'
-      console.log(data);
-    else if ( data === 'RIGHT' )
-    //command = 'echo 1 > ' + path + 'gpio11/value'
-      console.log(data);
-    else if ( data === 'STOPUP' )
-    //command = 'echo 0 > ' + path + 'gpio7/value'
-      console.log(data);
-    else if ( data === 'STOPDOWN' )
-    //command = 'echo 0 > ' + path + 'gpio8/value'
-      console.log(data);
-    else if ( data === 'STOPLEFT' )
-    //command = 'echo 0 > ' + path + 'gpio9/value'
-      console.log(data);
-    else if ( data === 'STOPRIGHT' )
-    //command = 'echo 0 > ' + path + 'gpio11/value'
-      console.log(data);
-    else
-      console.log(data);
+    if ( data === 'UP' ){
+      exec_command( 'echo 1 > ' + path + 'gpio2/value' );
+      exec_command( 'echo 1 > ' + path + 'gpio4/value' );
+    }else if ( data === 'DOWN' ){
+      exec_command( 'echo 1 > ' + path + 'gpio3/value' );
+      exec_command( 'echo 1 > ' + path + 'gpio14/value' );
+    }else if ( data === 'LEFT' ){
+      exxec_command( command = 'echo 1 > ' + path + 'gpio4/value' );
+    }else if ( data === 'RIGHT' ){
+      exec_command( 'echo 1 > ' + path + 'gpio14/value' );
+    }else if ( data === 'STOPUP' ){
+      exec_command( 'echo 0 > ' + path + 'gpio2/value' );
+      exec_command( 'echo 0 > ' + path + 'gpio4/value' );
+    }else if ( data === 'STOPDOWN' ){
+      exec_command( 'echo 0 > ' + path + 'gpio3/value' );
+      exec_command( 'echo 0 > ' + path + 'gpio14/value' );
 
-
-    // Execute the command to send the signal to the car's remote.
-    exec(command, function (error, stdout, stderr)
-    {
-      if (error !== null)
-        console.log('exec error: ' + error);
-    })
+    }else if ( data === 'STOPLEFT' ){
+      exec_command( 'echo 0 > ' + path + 'gpio4/value' );
+    }else if ( data === 'STOPRIGHT' ){
+      exec_command( 'echo 0 > ' + path + 'gpio14/value' );
+    }
 
   })
-
 });
 
 
+// Execute the command to send the signal to the car's remote.
+function exec_command( command ){
+  exec(command, function (error, stdout, stderr){ if (error !== null)
+    console.log('exec error: ' + error);
+  })
+}
+
+
+
+
 function stopStreaming(socket) {
-    delete sockets[socket.id];
-    console.log('Client desconected');
-    running_camera = false;
-    // no more sockets, kill the stream
-    if (Object.keys(sockets).length == 0) {
-      if (ffmpeg_command) ffmpeg_command.kill();
-    }
+  delete sockets[socket.id];
+  console.log('Client desconected');
+  running_camera = false;
+  // no more sockets, kill the stream
+  if (Object.keys(sockets).length == 0) {
+    if (ffmpeg_command) ffmpeg_command.kill();
+  }
 }
 
 
@@ -103,35 +100,35 @@ function startStreaming(socket) {
 
   if (running_camera == false){
     console.log('Starting streaming....');
-    var args = ["-f", "video4linux2", "-i", "/dev/video2", "-s", "300x150","-f","mjpeg", "pipe:1"]
+    var args = ["-f", "video4linux2", "-i", "/dev/video0", "-s", "300x150","-f","mjpeg", "pipe:1"]
     ffmpeg_command = require('child_process').spawn("ffmpeg", args);
     running_camera = true
   }
 
-	ffmpeg_command.on('error', function(err, stdout, stderr) {
-	  console.log("ffmpeg stdout:\n" + stdout);
-	  console.log("ffmpeg stderr:\n" + stderr);
-	  throw err;
+  ffmpeg_command.on('error', function(err, stdout, stderr) {
+    console.log("ffmpeg stdout:\n" + stdout);
+    console.log("ffmpeg stderr:\n" + stderr);
+    throw err;
     running_camera = false
   });
 
 
-	ffmpeg_command.on('close', function (code) {
-		console.log('ffmpeg exited');
+  ffmpeg_command.on('close', function (code) {
+    console.log('ffmpeg exited');
     running_camera = false
   });
 
-	/*
-	ffmpeg_command.stderr.on('data', function (data) {
-		console.log('stderr: ' + data);
-	});
-  */
+  /*
+   ffmpeg_command.stderr.on('data', function (data) {
+   console.log('stderr: ' + data);
+   });
+   */
 
-	ffmpeg_command.stdout.on('data', function (data) {
-		//console.log("stream data");
-		var frame = new Buffer(data).toString('base64');
-		socket.emit('canvas',frame);
-	});
+  ffmpeg_command.stdout.on('data', function (data) {
+    //console.log("stream data");
+    var frame = new Buffer(data).toString('base64');
+    socket.emit('canvas',frame);
+  });
 
 }
 
@@ -139,47 +136,42 @@ function startStreaming(socket) {
 
 function initPins()
 {
-	// Enable control of the Raspberry Pi's gpio pins.
-	for (var pin in pins)
-	{
-		console.log('Creating port ' + pins[pin] + '...');
+  // Enable control of the Raspberry Pi's gpio pins.
+  for (var pin in pins)
+  {
+    console.log('Creating port ' + pins[pin] + '...');
 
-		// The command first checks whether the port already exists.
-		var command = 'if (! [ -f ' + path + 'gpio' + pins[pin] + '/direction ]); then ' +
-			'echo ' + pins[pin] + ' > ' + path + 'export; fi';
+    // The command first checks whether the port already exists.
+    var command = 'if (! [ -f ' + path + 'gpio' + pins[pin] + '/direction ]); then ' +
+      'echo ' + pins[pin] + ' > ' + path + 'export; fi';
 
-		// Create the ports using Raspbian's command line.
-		exec(command, function(error, stdout, stderr)
-		{
-			if (error === null)
-				console.log('Successfully created port.');
-			else
-				console.log('Error when creating port: ' + error + ' (' + stderr + ').');
-		})
-	}
+    // Create the ports using Raspbian's command line.
+    exec(command, function(error, stdout, stderr)
+    {
+      if (error === null)
+        console.log('Successfully created port.');
+      else
+        console.log('Error when creating port: ' + error + ' (' + stderr + ').');
+    })
+  }
 
-	// Configure the Raspberry Pi's gpio pins as output ports which enables signals
-	// to be sent to the car's remote control.
-	for (var pin in pins)
-	{
-		console.log('Configuring port ' + pins[pin] + '...');
+  // Configure the Raspberry Pi's gpio pins as output ports which enables signals
+  // to be sent to the car's remote control.
+  for (var pin in pins)
+  {
+    console.log('Configuring port ' + pins[pin] + '...');
 
-		// The command configures the pin as an output port.
-		var command = 'echo out > ' + path + 'gpio' + pins[pin] + '/direction';
+    // The command configures the pin as an output port.
+    var command = 'echo out > ' + path + 'gpio' + pins[pin] + '/direction';
 
-		// Configure the ports using Raspbian's command line.
-		exec(command, function(error, stdout, stderr)
-		{
-			if (error === null)
-				console.log('Successfully configured pin.');
-			else
-				console.log('Error when configuring port: ' + error + ' (' + stderr + ').');
-		})
-	}
+    // Configure the ports using Raspbian's command line.
+    exec(command, function(error, stdout, stderr)
+    {
+      if (error === null)
+        console.log('Successfully configured pin.');
+      else
+        console.log('Error when configuring port: ' + error + ' (' + stderr + ').');
+    })
+  }
 }
-
-
-
-
-
 
