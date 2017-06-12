6544a5b8fe0b1115a10230973664101a88e10a38
Manuel López Urbina <m_lopez_servext_rsi@cajarural.com>
2017-06-12 20:01:33 +0200

    documentation last changes

--- api/controllers/SliderController.js
+++ api/controllers/SliderController.js
@@ -78,34 +78,62 @@ module.exports = {
 
   destroy: function (req, res, next) {
     if (req.xhr) {
-      slider.destroy({id: req.param('id')}).exec(function deleteslider(err) {
-        console.log('The slider has been deleted');
-
-        //Si hay error
-        if (err) {
-          console.log(err);
-          return res.next(err);
-        }
-
+      Slider.destroy({id: req.param('id')}).exec(function deleteslider(err) {
+        if(err) return res.badRequest(err);
         return res.ok({id: req.param('id')});
       });
     } else {
-      err = 'Ajax call';
-      return res.badRequest(err);
+      return res.badRequest('Ajax call');
     }
-
   },
 
 
   edit: function(req, res, next){
-    slider.findOne(req.param('id'), function foundslider(err, slider){
-      if(err) return next(err);
-      if(!slider) return next();
+    Slider.findOne(req.param('id'), function foundslider(err, slider){
+      if(err) return res.badRequest(err);
+      if(!slider) return res.badRequest(__('not_found'));
 
-        res.view({slider: slider});
+      return res.render('slider/edit.ejs', {
+        slider: slider,
+        layout: false
+      });
     });
   },
 
+
+  update: function(req, res, next){
+    if (req.xhr) {
+        var vertical = req.param('vertical') == 'true' ? true : false
+
+        var sliderObj = {
+          name: req.param('name'),
+          code: req.param('code'),
+          min: req.param('min'),
+          max: req.param('max'),
+          value: req.param('value'),
+          step: req.param('step'),
+          length: req.param('length'),
+          vertical: vertical
+        };
+
+
+        Slider.update(req.param('id'), sliderObj, function eventUpdated(err, slider) {
+          if (err) return res.badRequest(err);
+
+          FlashService.success(req, 'Slider has been updated');
+
+          Slider.findOne(req.param('id')).exec(function (err, slider) {
+            return res.render('interface/_slider.ejs', {
+              slider: slider,
+              layout: false
+            });
+          });
+
+        });
+    } else {
+      return res.badRequest('Ajax call');
+    }
+  },
 
   index: function(req, res, next) {
     slider.find({interface_owner: req.session.User.id}).exec(function (err, sliders) {
